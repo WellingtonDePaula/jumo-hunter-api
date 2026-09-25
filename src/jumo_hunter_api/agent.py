b5e24613524876dbd6774bf3cf88e8b3217e1b5f
Wellz <owellingtondepaula@gmail.com>
2026-09-25 17:41:25 -0300

    Testing get route

--- src/jumo_hunter_api/agent.py
+++ src/jumo_hunter_api/agent.py
@@ -1,8 +1,16 @@
+from flask.views import MethodView
 from flask_smorest import Blueprint
 
 blp = Blueprint(
     "agent",
     __name__,
-    url_prefix="agent",
+    url_prefix="/agent",
     description="Route for agent operations"
     )
+
+@blp.route("/")
+class Agent(MethodView):
+    
+    blp.response(200)
+    def get(self):
+        return "nice"
